ce79aecb441eaf8d82292dfa1222d8f6c8fc4787
ryanp2dev <65263907+ryanp2dev@users.noreply.github.com>
2024-06-27 08:02:22 -0400

    instrucoes

---
+++ exercicios/exercicios-0016.php
@@ -0,0 +1,13 @@
+<?php 
+
+/*
+16 – Um atleta precisa saber qual sua categoria ao disputar o campeonato.
+Implemente um programa para receber o peso, e imprima a categoria de
+acordo com a tabela:
+
+    Peso    CAtegoria
+    <=50    novato
+    >       Profissional 
+    
+Nome do projeto – exercicio0016.php
+*/
