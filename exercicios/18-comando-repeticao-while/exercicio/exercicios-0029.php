7123dc4a78ed5ec719ce85ac9865fd65dd4fcdea
ryanp2dev <65263907+ryanp2dev@users.noreply.github.com>
2024-06-28 21:15:21 -0400

    exercios da aula 18

---
+++ exercicios/18-comando-repeticao-while/exercicio/exercicios-0029.php
@@ -0,0 +1,14 @@
+<?php 
+
+/*
+29 – Implemente um programa que receba dois valores quaisquer e imprime
+todos os valores intermediários a ele, usando o WHILE
+
+
+*/
+
+$a = 0;
+$b = 10;
+while ($a <= $b) {
+    echo $a++ . "<br>";
+}
---
+++ exercicios/exercicios-0028.php
@@ -0,0 +1,14 @@
+<?php 
+
+/*
+28 – Implemente o programa que receba um valor qualquer e imprima os
+valores de 0 até o valor recebido, usando o WHILE.
+Nome do projeto – exercicio0028.php
+
+*/
+
+$valor = 100;
+$a = 0;
+while ($a <= $valor) {
+    echo $a++ . "<br>";
+}
